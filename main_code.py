d12cdbdd89ebe36cc291b525917207c6c050234d
Rafa <rafalaz01@gmail.com>
2019-07-27 18:35:47 +0200

    Completed comments. Added Baseline result.

--- main_code.py
+++ main_code.py
@@ -1,5 +1,6 @@
 # Import needed packages
 import numpy as np
+import pandas as pd
 import random
 from collections import deque
 from sklearn import preprocessing
@@ -10,7 +11,7 @@ from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
 import matplotlib.pyplot as plt
-
+from sklearn.dummy import DummyRegressor
 
 class BitcoinPredictorPipeline:
     """ This class represents a whole criptocurrency price predictor. Parameters like currency,
@@ -26,7 +27,7 @@ class BitcoinPredictorPipeline:
         self.test_y = None
         self.model = None
 
-    def download_data(self, criptocoin='BTC', currency='USD'):
+    def download_data(self, criptocoin='BTC', currency='EUR'):
         # Alpha Vantage API
         api_key = "J41D51QYZT0JLZ99"
         # This API outputs a dataframe with the open, low, high and close values in the "market" currency
@@ -39,26 +40,24 @@ class BitcoinPredictorPipeline:
                                     "2a. high (EUR)": "high",
                                     "1a. open (EUR)": "open",
                                     "4a. close (EUR)": "close",
-                                    "5. volume": "volume"})
-        self.data = data
-
-    def preprocess_basic(self):
-        # Set time as index so we can join them on this shared time
-        self.data.set_index("date", inplace=True)
+                                    "5. volume": "volume" })
 
         # Keep only the "close" and "volume" columns
-        self.data = self.data[["close", "volume"]]
+        self.data = data[["close", "volume"]]
 
-        # Fill NaN's
+    def separate_train_test(self):
+
+        # Fill possible NaN's. (Non-existant)
         self.data.fillna(method="ffill", inplace=True)
-        # TODO why drop is necessary if we already filled nans?
-        self.data.dropna(inplace=True)
-
+        # Create target variable by shifting the closing value as many days as "future_distance"
         self.data['future'] = self.data['close'].shift(-self.future_distance)
         self.data.dropna(inplace=True)
-        times = sorted(self.data.index.values)
-        last_records = sorted(self.data.index.values)[-int(0.1 * len(times))]
 
+        # Sort values
+        times = sorted(self.data.index.values)
+
+        # Separate last 10% for test values and rest for training
+        last_records = sorted(self.data.index.values)[-int(0.1 * len(times))]
         test_df = self.data[(self.data.index >= last_records)]
         train_df = self.data[(self.data.index < last_records)]
 
@@ -67,44 +66,47 @@ class BitcoinPredictorPipeline:
 
     def preprocess_df(self, df):
 
+        # Scale values between 0 and 10.
         df = self.__scale(df)
 
-        sequential_data = []  # this is a list that will CONTAIN the sequences
+        # This list will contain all the sequences
+        sequential_data = []
+        # This deque structure will hold each sequence
         prev_days = deque(maxlen=self.past_length)
 
-        for i in df.values:  # iterate over the values
-            prev_days.append([n for n in i[:-1]])  # store all but the target
-            if len(prev_days) == self.past_length:  # make sure we have 60 sequences!
-                sequential_data.append([np.array(prev_days), i[-1]])  # append those bad boys!
+        for i in df.values:  # iterate over the rows
+            # Append close and volume columns
+            prev_days.append([n for n in i[:-1]])
+            # If we recorded all the values of the sequence (self.past_length) we add it to sequential_data
+            if len(prev_days) == self.past_length:
+                # Each element of sequential_data contains all independent columns for the last "past_lenght" elements
+                # and the target value.
+                sequential_data.append([np.array(prev_days), i[-1]])
 
-        random.shuffle(sequential_data)  # shuffle for good measure.
-
-        print(np.array(sequential_data).shape)
-
-        random.shuffle(
-            sequential_data)  # another shuffle, so the model doesn't get confused with all 1 class then the other.
-
+        # As each element of "sequential_data" can now be independently used for train or test, we can (must) shuffle it
+        random.shuffle(sequential_data)
         x = []
         y = []
-
-        for seq, target in sequential_data:  # going over our new sequential data
-            x.append(seq)  # X is the sequences
-            y.append(target)  # y is the targets/labels (buys vs sell/notbuy)
-
-        print(x[0])
-        print(y[0])
+        for seq, target in sequential_data:
+            x.append(seq)
+            y.append(target)
+        # Return the sequences and their target values
         return np.array(x), y  # return X and y...and make X a numpy array!
 
     def __scale(self, df):
-        for col in df.columns:  # go through all of the columns
-            df[col] = preprocessing.scale(df[col].values)  # scale between 0 and 1.
-        return df
+        # go through all of the columns
+        scaler = preprocessing.MinMaxScaler(feature_range=(0, 10))
+        scaler.fit(df.values)
+        df = scaler.transform(df.values)
+        return pd.DataFrame(df)
 
     def build_model(self):
+        # Let's build the model:
         model = Sequential()
         model.add(LSTM(128, input_shape=(pipeline.train_x.shape[1:]), return_sequences=True))
         model.add(Dropout(0.2))
-        model.add(BatchNormalization())  # normalizes activation outputs
+        # Normalize activation outputs:
+        model.add(BatchNormalization())
 
         model.add(LSTM(128, return_sequences=True))
         model.add(Dropout(0.1))
@@ -114,59 +116,59 @@ class BitcoinPredictorPipeline:
         model.add(Dropout(0.2))
         model.add(BatchNormalization())
 
-        model.add(Dense(32, activation='relu'))
+        model.add(Dense(15, activation='relu'))
         model.add(Dropout(0.2))
 
         model.add(Dense(1, activation='linear'))
         # Model compile settings:
-
         opt = Adam(lr=0.001, decay=1e-6)
 
         # Compile model
         model.compile(
             loss='mse',
-            optimizer=opt,
-            metrics=['mape']
+            optimizer=opt
         )
 
         model.summary()
         self.model = model
 
     def train_model(self, batch_size=64, epochs=5):
-        name = f"{self.past_length}-SEQ-{self.future_distance}-PRED-{int(time.time())}"
-        tensorboard = TensorBoard(log_dir="logs/{}".format(name))
-
         history_fitting = self.model.fit(
             self.train_x, self.train_y,
             batch_size=batch_size,
             epochs=epochs,
-            validation_data=(self.test_x, self.test_y),
-            callbacks=[tensorboard])
+            validation_data=(self.test_x, self.test_y))
         return history_fitting
 
     def evaluate_visualize_model(self, history_records):
-        score = self.model.evaluate(self.test_x, self.test_y, verbose=0)
+        # Calculate and print the mse
+        score = self.model.evaluate(self.test_x, self.test_y, verbose=1)
+        print('Test mse:', score)
 
-        print('Validation mean_absolute_percentage_error:', score[1])
-        print('Validation mse:', score[0])
+        # Baseline model
+        baseline_median = np.median(self.train_y)
+        from sklearn.metrics import mean_squared_error
+        mse_baseline = mean_squared_error(np.asarray(self.test_y), np.full(shape=len(self.test_y), fill_value=baseline_median, dtype=np.float))
 
         # Loss per epoch
         plt.plot(history_records.history['loss'])
         plt.plot(history_records.history['val_loss'])
+        plt.axhline(y=mse_baseline, color='r', linestyle='-')
         plt.title('Loss per epoch')
         plt.ylabel('Loss')
         plt.xlabel('Epoch')
-        plt.legend(['Train', 'Validation'], loc='upper left')
+        plt.legend(['Train', 'Test'], loc='upper left')
         plt.show()
 
 # Instantiate the class
-pipeline = BitcoinPredictorPipeline(40, 2)
+pipeline = BitcoinPredictorPipeline(30, 1)
 
 # Download the data
 pipeline.download_data()
-pipeline.preprocess_basic()
+pipeline.separate_train_test()
 print("Training records: %s" % len(pipeline.train_x))
 print("Validation records: %s" % len(pipeline.test_x))
 pipeline.build_model()
 history = pipeline.train_model()
 pipeline.evaluate_visualize_model(history)
+
